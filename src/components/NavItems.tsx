8699fe6cf8a02330260fd42534851fcf95551db1
JuniorMira0 <j-unyor14@hotmail.com>
2024-10-02 19:19:01 -0300

    add hooks para fechar o nav link do header com clique e tecla escape

--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -1,16 +1,37 @@
 'use client';
 
 import { PRODUCT_CATEGORIES } from '@/config';
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import NavItem from './NavItem';
+import { useOnClickOutside } from '@/hooks/use-on-click-outside';
 
 const NavItems = () => {
   const [activeIndex, setActiveIndex] = useState<null | number>(null);
 
+  /* add o evento do teclado ao pressionar a tecla escape fecha o nav*/
+  useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+      if (e.key === 'Escape') {
+        setActiveIndex(null);
+      }
+    };
+
+    document.addEventListener('keydown', handler);
+
+    return () => {
+      document.removeEventListener('keydown', handler);
+    };
+  }, []);
+
   const isAnyOpen = activeIndex != null;
 
+  /* hook para o usuario clicar fora da nav do header e fechar */
+  const navRef = useRef<HTMLDivElement | null>(null);
+
+  useOnClickOutside(navRef, () => setActiveIndex(null));
+
   return (
-    <div className="flex gap-4 h-full">
+    <div className="flex gap-4 h-full" ref={navRef}>
       {PRODUCT_CATEGORIES.map((category, i) => {
         const handleOpen = () => {
           if (activeIndex === i) {
