74a6ab72cfe35f0dbe8401607f5436685e632039
JuniorMira0 <j-unyor14@hotmail.com>
2024-10-02 22:51:17 -0300

    Cria função para formatar moeda

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,27 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+/* Faz a formatação do preço usando a API Intl e tnm convertente de string para number */
+export function formatPrice(
+  price: number | string,
+  options: {
+    currency?: 'USD' | 'EUR' | 'GBP' | 'BDT' | 'BRL',
+    notation?: Intl.NumberFormatOptions['notation']
+  } = {}
+) {
+  const {currency = 'USD', notation = 'compact'} = options
+
+  const numericPrice = typeof price === 'string' ? parseFloat(price) : price;
+  if (isNaN(numericPrice)) {
+    throw new Error('Invalid price value');
+  }
+
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency,
+    notation,
+    maximumFractionDigits: 2
+  }).format(numericPrice)
+}
+
